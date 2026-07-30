5ca8db3bf4e831b4c344a878e98290554ac3e445
BondarVladyslav <1vladbondar1@gmail.com>
2026-07-30 22:22:57 +0300

    fixed: pagination without ordering problem

--- CheckSiteOut/dashboard/models.py
+++ CheckSiteOut/dashboard/models.py
@@ -5,6 +5,8 @@ UserModel = get_user_model()
 class Site(models.Model):
     owners = models.ManyToManyField(UserModel, related_name='sites')
     link = models.TextField(unique=True)
+    class Meta:
+        ordering = ['-id']
 
 class SiteResponse(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='responses')

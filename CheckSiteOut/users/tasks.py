123e043af98939bd65c3fb5c95758316e8e46ea0
BondarVladyslav <1vladbondar1@gmail.com>
2026-08-19 01:16:16 +0300

    Fixed unretrieble sending message

--- CheckSiteOut/users/tasks.py
+++ CheckSiteOut/users/tasks.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import logging
 import os
 from django.utils import timezone
 from dotenv import load_dotenv
@@ -10,16 +11,21 @@ load_dotenv()
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+logger = logging.getLogger(__name__)
 
 
 @app.task(bind=True, max_retries=3, retry_backoff=10)
 def send_report_message(self, telegram_id, text):
-    response = requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={"chat_id": telegram_id, "text": text},
-        timeout=10,
-    )
-    response.raise_for_status()
+    try:
+        response = requests.post(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            json={"chat_id": telegram_id, "text": text},
+            timeout=10,
+        )
+        response.raise_for_status()
+    except:
+        logger.log("Lost telegram message for telegram_id %s", telegram_id)
+        self.retry()
 
 
 @app.task
